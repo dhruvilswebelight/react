608451d916f2006adb53a1ccd8044787cc13d49e
dhruvilS_webelight <dhruvils@webelight.co.in>
2022-05-17 10:02:44 +0530

    practical

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
+// import React, { createContext } from "react";
+// import ComA from "./context/ComA";
+
+// const FirstName = createContext();
+// const LastName = createContext();
+
+// const App = () => {
+//   return (
+//     <>
+//       <FirstName.Provider value={"Dhruvil"}>
+//         <LastName.Provider value={"Suhagiya"}>
+//           <ComA />
+//         </LastName.Provider>
+//       </FirstName.Provider>
+//     </>
+//   );
+// };
+
+// export default App;
+// export { FirstName, LastName };
+
+//combine context
+
 import React, { createContext } from "react";
 import ComA from "./context/ComA";
 
-const FirstName = createContext();
-const LastName = createContext();
+const user = {
+  FirstName: "Dhruvil",
+  LastName: "Suhagiya",
+};
+const fullName = createContext({ user });
 
 const App = () => {
   return (
     <>
-      <FirstName.Provider value={"Dhruvil"}>
-        <LastName.Provider value={"Suhagiya"}>
-          <ComA />
-        </LastName.Provider>
-      </FirstName.Provider>
+      <fullName.Provider value={user}>
+        <ComA />
+      </fullName.Provider>
     </>
   );
 };
 
 export default App;
-export { FirstName, LastName };
+export { fullName };
--- src/context/ComC.jsx
+++ src/context/ComC.jsx
@@ -1,24 +1,46 @@
+// import React from "react";
+// import { FirstName, LastName } from "../App";
+
+// const ComC = () => {
+//   return (
+//     <>
+//       <FirstName.Consumer>
+//         {(fname) => {
+//           return (
+//             <LastName.Consumer>
+//               {(lname) => {
+//                 return (
+//                   <h1>
+//                     My name is {fname} {lname}
+//                   </h1>
+//                 );
+//               }}
+//             </LastName.Consumer>
+//           );
+//         }}
+//       </FirstName.Consumer>
+//     </>
+//   );
+// };
+// export default ComC;
+
+//Combine context
+
 import React from "react";
-import { FirstName, LastName } from "../App";
+import { fullName } from "../App";
 
 const ComC = () => {
   return (
     <>
-      <FirstName.Consumer>
-        {(fname) => {
+      <fullName.Consumer>
+        {(user) => {
           return (
-            <LastName.Consumer>
-              {(lname) => {
-                return (
-                  <h1>
-                    My name is {fname} {lname}
-                  </h1>
-                );
-              }}
-            </LastName.Consumer>
+            <h1>
+              My name is {user.FirstName} {user.LastName}
+            </h1>
           );
         }}
-      </FirstName.Consumer>
+      </fullName.Consumer>
     </>
   );
 };
